6f27efe84564a89473abe4e7f342b7308f00b1f2
Yuriy Tkach <yuriytkach@gmail.com>
2023-02-26 22:35:27 +0200

    Increat gasLimit for develop network

--- truffle-config.js
+++ truffle-config.js
@@ -64,21 +64,22 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
+    develop: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     // },
     //
     // An additional network, but with some advanced options…
     // advanced: {
     //   port: 8777,             // Custom port
     //   network_id: 1342,       // Custom network
-    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      gasLimit: 9000000,
+       gas: 85000000,           // Gas sent with each transaction (default: ~6700000)
+       gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
     //   from: <address>,        // Account to send transactions from (default: accounts[0])
     //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-    // },
+    },
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
